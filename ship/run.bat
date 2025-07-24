5b501ca1af00b5975f3bde30959a93ec6004d03b
Adekoya Adekunle <adekunleadekoya@gmail.com>
2025-07-25 00:05:10 +0100

    Updated UI, Updated run.bat

--- ship/run.bat
+++ ship/run.bat
@@ -1,7 +1,6 @@
 @echo off
 REM Set the path to the JavaFX SDK relative to the batch file
 set JAVAFX_PATH=%~dp0lib
-set JAVAFX_PATH=C:\Dekun - files\RUN - 2024\Biometric Project\BioApp\PreRegisterCards\my-javafx-app\ship\lib
 
 REM Echo the JavaFX path to verify it
 echo JAVAFX_PATH is set to: "%JAVAFX_PATH%"
